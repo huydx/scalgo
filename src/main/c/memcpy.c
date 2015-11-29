61c9b3fc6ef78bd2fffee267bfdac34de598147f
huydx <doxuanhuy@gmail.com>
2015-11-29 07:36:27 +0000

    add memcpy.c

--- src/main/c/memcpy.c
+++ src/main/c/memcpy.c
@@ -3,11 +3,9 @@
 void *memcpy(void *dst, void const *src, size_t len) {
   long * plDst = (long *)dst;
   long const *plSrc = (long const *)src;
-  if (!(src & 0xFFFFFFFC) && !(dst & 0xFFFFFFFC)) {
-    while (len >= 4) {
-      *plDst ++= *plSrc++;
-      len -= 4;
-    }
+  while (len >= 4) {
+    *plDst ++= *plSrc++;
+    len -= 4;
   }
 
   char * pcDst = (char *) plDst;
@@ -21,5 +19,12 @@ void *memcpy(void *dst, void const *src, size_t len) {
 }
 
 void main(char** args, int argv) {
-
+  char str1[128] = {1,1,1,1,1,1,1};
+  char str2[] = "ab\0cde";        /* 途中に空文字のある文字列 */
+  int i;
+  memcpy(str1, str2, 5);
+  printf("コピー後中身の確認\n");
+  for (i=0; i<7; i++)
+    printf("%#x ",str1[i]);
+  printf("\n");
 }
